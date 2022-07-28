573d1cb2ef272628c9b3768663c3c9cdef9b2c88
Aimal Amiri <meaimal@hotmail.com>
2022-07-28 18:42:09 +0430

    solver_rspec test code cleanup

--- rspec/solver_spec.rb
+++ rspec/solver_spec.rb
@@ -1,18 +1,19 @@
 require_relative '../solver'
 
 describe 'Solver' do
+  before(:each) do
+    @solver = Solver.new
+  end
+
   it 'factorial method should return 120 for 5' do
-    solver = Solver.new
-    expect(solver.factorial(5)).to eq(120)
+    expect(@solver.factorial(5)).to eq(120)
   end
 
   it 'factorial method should raise an exception if the parameter is a negative value' do
-    solver = Solver.new
-    expect { solver.factorial(-5) }.to raise_exception(ArgumentError)
+    expect { @solver.factorial(-5) }.to raise_exception(ArgumentError)
   end
 
   it 'method factorial returns 1 for 0' do
-    solver = Solver.new
-    expect(solver.factorial(0)).to eq(1)
+    expect(@solver.factorial(0)).to eq(1)
   end
 end
