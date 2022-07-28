d532546993575fc5aeea5cbc0a99e43011b452de
Aimal Amiri <meaimal@hotmail.com>
2022-07-28 18:40:59 +0430

    Factorial will return 1 for 0

--- solver.rb
+++ solver.rb
@@ -2,7 +2,7 @@ class Solver
   def factorial(num)
     raise ArgumentError, 'The num argument should be a positive number' if num.negative?
 
-    return 1 if num == 1
+    return 1 if [1, 0].include?(num)
 
     num * factorial(num - 1)
   end
