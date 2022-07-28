6ae005c26d086151db0ff9a275028ec58343a165
Aimal Amiri <meaimal@hotmail.com>
2022-07-28 18:38:11 +0430

    factorial will raise an exception for a negative value

--- solver.rb
+++ solver.rb
@@ -1,5 +1,7 @@
 class Solver
   def factorial(num)
+    raise ArgumentError, 'The num argument should be a positive number' if num.negative?
+
     return 1 if num == 1
 
     num * factorial(num - 1)
